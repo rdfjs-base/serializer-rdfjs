7cb12072ddf9e4b3ce3de6e89ee2c3e586130385
tpluscode <tpluscode@users.noreply.github.com>
2020-07-17 15:59:33 +0200

    feat: support output as commonjs/es modules/typescript

--- test/test.js
+++ test/test.js
@@ -2,6 +2,7 @@ const { strictEqual } = require('assert')
 const getStream = require('get-stream')
 const intoStream = require('into-stream')
 const { describe, it } = require('mocha')
+const { expect } = require('chai')
 const toCanonical = require('rdf-dataset-ext/toCanonical')
 const rdf = require('@rdfjs/data-model')
 const sinkTest = require('@rdfjs/sink/test')
@@ -78,6 +79,25 @@ describe('Serializer', () => {
       const result = eval(code)(rdf) /* eslint-disable-line no-eval */
 
       strictEqual(toCanonical(result), toCanonical(quads))
+    })
+
+    describe('should generate module when the type is', () => {
+      [undefined, 'commonjs', 'esm', 'ts'].forEach(module => {
+        it(module || 'undefined', function () {
+          const quads = [
+            rdf.quad(
+              rdf.blankNode(),
+              rdf.namedNode('http://example.org/predicate1'),
+              rdf.literal('object')
+            )
+          ]
+
+          const serializer = new Serializer({ module })
+          const code = serializer.transform(quads)
+
+          expect(code).to.matchSnapshot(this)
+        })
+      })
     })
   })
 })
