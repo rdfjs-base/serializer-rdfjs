835253e36af801414c7b9ef5eb70892a583a3a21
bergi <bergi@axolotlfarm.org>
2021-12-18 20:51:23 +0100

    fix: updated readme and extracted example

---
+++ examples/simple.js
@@ -0,0 +1,26 @@
+import { Readable } from 'stream'
+import rdf from '@rdfjs/data-model'
+import Serializer from '../index.js'
+
+const serializer = new Serializer()
+const input = new Readable({
+  objectMode: true,
+  read: () => {
+    input.push(rdf.quad(
+      rdf.namedNode('http://example.org/sheldon-cooper'),
+      rdf.namedNode('http://schema.org/givenName'),
+      rdf.literal('Sheldon')))
+    input.push(rdf.quad(
+      rdf.namedNode('http://example.org/sheldon-cooper'),
+      rdf.namedNode('http://schema.org/familyName'),
+      rdf.literal('Cooper')))
+    input.push(rdf.quad(
+      rdf.namedNode('http://example.org/sheldon-cooper'),
+      rdf.namedNode('http://schema.org/knows'),
+      rdf.namedNode('http://example.org/amy-farrah-fowler')))
+    input.push(null)
+  }
+})
+
+const output = serializer.import(input)
+output.pipe(process.stdout)
